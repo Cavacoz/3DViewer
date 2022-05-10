ea1a22cead9708194ee9ae8e19faeff9cfb96100
rmpco-iscteiul <56404528+rmpco-iscteiul@users.noreply.github.com>
2022-05-10 02:28:20 +0100

    Meti direito as calls das funções das classes(objectos) ObjectLoader.scala e OctreeEditor.scala.

--- src/ObjectLoader.scala
+++ src/ObjectLoader.scala
@@ -1,36 +1,26 @@
-import javafx.application.Application
-import javafx.geometry.Insets
-import javafx.scene.paint.PhongMaterial
-import javafx.scene.shape._
-import javafx.scene.transform.{Rotate, Translate}
+import javafx.scene.paint.{Color, PhongMaterial}
+import javafx.scene.shape.{Cylinder, Shape3D}
 import javafx.scene.{Group, Node}
-import javafx.stage.Stage
-import javafx.geometry.Pos
-import javafx.scene.layout.StackPane
-import javafx.scene.paint.Color
-import javafx.scene.{PerspectiveCamera, Scene, SceneAntialiasing, SubScene}
 
-import scala.collection.mutable.ListBuffer
 import scala.io.Source
 
-case class ObjectLoader() {
+object ObjectLoader {
 
   type Point = (Double, Double, Double)
   type Size = Double
   type Placement = (Point, Size)
   type Section = (Placement, List[Node])
 
-  def loadFromTextFile(file: String, root: Group, list: List[Shape3D]): List[Shape3D] = {
+  def loadFromTextFile(file: String, root: Group): List[Shape3D] = {
 
     var list3D:List[Shape3D] = List()
     val materialColour = new PhongMaterial()
-    println(file)
     val bufferedSource = Source.fromFile(file)
 
-    for (line <- bufferedSource.getLines()){
+    for (line <- bufferedSource.getLines){
+      val linha = line.split(" ")
+      val colour = linha(1).split(",")
 
-      val linha: Array[String] = line.split(" ")
-      val colour: Array[String] = linha(1).split(",")
       if(linha(0).equals("Cylinder")) {
         val cylinder = new Cylinder(0.5, 1, 10)
         cylinder.setTranslateX(linha(2).toInt)
@@ -43,13 +33,12 @@ case class ObjectLoader() {
         materialColour.setDiffuseColor(Color.rgb(colour(0).toInt, colour(1).toInt, colour(2).toInt))
         cylinder.setMaterial(materialColour)
 
-        //list3D = list :+ cylinder
         list3D = addInsideList(list3D, cylinder)
+
         root.getChildren.add(cylinder)
       }
     }
     bufferedSource.close
-    println(list3D)
     list3D
   }
 
@@ -59,4 +48,5 @@ case class ObjectLoader() {
       case x::xs => x::addInsideList(xs, objeto)
     }
   }
+
 }
